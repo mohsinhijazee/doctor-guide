1371d965ba818ccf34397d034a9b8daf746d5d79
Mohsin Hijazee <m_hijazee@yahoo.com>
2010-01-31 06:40:21 +0500

    UPDATE: Now time is being stored as float so that comparisons can be made

--- helloworld.py
+++ helloworld.py
@@ -13,20 +13,74 @@ from google.appengine.ext.db import djangoforms
 
 
 specializations = set(['Neurology,', 'General', 'Psychology', 'Dental']);
+hours = ["%02d" % h for h in range(1,13)]
+mins = ["%02d" % h for h in range(0,60)]
 
+#TODO: Doctor deletion
+#TODO: What should be the default values for time boxes?
+#TODO: Calculating the time difference
+#      Time difference should be stored as float with following procedure
+# time_to_store = hour + min/60
+# if pm:
+#   time_to_store += 12
+
+
+def time_as_number(t):
+    temp = t.split(':')
+    
+    h = float(temp[0])
+    
+    if h == 12 and temp[2].lower() == 'am':
+        h = 0
+        
+    if h < 12 and temp[2].lower() == 'pm':
+        h += 12
+        
+    h = h + float(temp[1])/60.0
+
+    return h
+
+
+def time_as_string(num):
+    h = int(num)
+    m = round((num - int(num)) * 60.0)
+    
+    ampm = 'AM'
+    
+    if h >= 12:
+      h -= 12
+      ampm = 'PM'
+      
+    return "%02d:%02d:%s" % (h,m,ampm)
+    
+    
 #FIXME: Timings must be datetime.datetime
 class Doctor(db.Model):
     name              = db.StringProperty('Name', required=True)
     specialization    = db.StringProperty('Specialization',
                          default='General', required=True, 
                          choices=specializations)
-    sits_from         = db.StringProperty('From')
-    sits_upto         = db.StringProperty('Up to')
+    sits_from         = db.FloatProperty('From')
+    sits_upto         = db.FloatProperty('Up to')
     address           = db.PostalAddressProperty('Address')
     fee               = db.IntegerProperty('Fee')
     phone             = db.PhoneNumberProperty('Phone')
     email             = db.EmailProperty('Email')
     rating            = db.RatingProperty('Rating')
+    
+    def sits_from_string(self, time_as_str=None):
+        if time_as_str is None:
+           return time_as_string(self.sits_from)
+        else:
+           self.sits_from = time_as_number(time_as_str)
+           
+    def sits_upto_string(self, time_as_str=None):
+        if time_as_str is None:
+           return time_as_string(self.sits_upto)
+        else:
+           self.sits_upto = time_as_number(time_as_str)
+    
+    
     
 
 # This is just temporary as we will not be generating the form
@@ -46,7 +100,9 @@ class DoctorsListings(webapp.RequestHandler):
         
         template_values = {'doctors': doctors, 
                            'specializations' : specializations,
-                           'form': DoctorForm()}
+                           'hours': hours,
+                           'mins': mins
+                           }
         
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         
@@ -62,8 +118,16 @@ class RegisterDoctor(webapp.RequestHandler):
         
         doctor= Doctor(name=self.request.get('name'),  specialization=self.request.get('specialization'))
         
-        doctor.sits_from = self.request.get('sits_from')
-        doctor.sits_upto = self.request.get('sits_upto')
+        from_time = "%s:%s:%s" % (self.request.get('sits_from_hour'),
+                                  self.request.get('sits_from_min'),
+                                  self.request.get('sits_from_ampm'))
+                                  
+        upto_time = "%s:%s:%s" % (self.request.get('sits_upto_hour'),
+                                  self.request.get('sits_upto_min'),
+                                  self.request.get('sits_upto_ampm'))
+
+        doctor.sits_from = float(time_as_number(from_time))
+        doctor.sits_upto = float(time_as_number(upto_time))
         doctor.address = db.PostalAddress(self.request.get('address'))
         doctor.fee = int(self.request.get('fee'))
         doctor.phone = db.PhoneNumber(self.request.get('phone'))
